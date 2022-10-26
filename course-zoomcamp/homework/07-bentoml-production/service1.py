8bcdeb17aed9a0bf0e39ad38cf00ea1ad0696381
plee <pllee@email.arizona.edu>
2022-10-27 02:36:26 +0800

    Add homework7

---
+++ course-zoomcamp/homework/07-bentoml-production/service1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import bentoml
+from bentoml.io import NumpyNdarray
+
+model_ref = bentoml.sklearn.get("mlzoomcamp_homework:qtzdz3slg6mwwdu5")
+#dv = model_ref.custom_objects['dictVectorizer']
+
+model_runner = model_ref.to_runner()
+
+svc = bentoml.Service("mlzoomcamp_homework_predictor1", runners=[model_runner])
+
+
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+async def classify(vector):
+    #vector = dv.transform(application_data)
+    prediction = await model_runner.predict.async_run(vector)
+    print(prediction)
+    
+    return prediction
